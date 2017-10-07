8bfc44884c1fdf226a1421bd41e25949e18754fb
ElenaBogdanova <elena_12_00@bk.ru>
2017-10-07 12:59:24 +0300

    add romb program

---
+++ romb.pas
@@ -0,0 +1,40 @@
+program romb1;
+var i,n,k :byte;
+
+begin
+   writeln ('print size (odd number)');
+   readln (n);
+   for i := 1 to n do
+      begin
+        writeln;
+        if (i = 1) or (i = n) then 
+          begin
+            for k := 1 to (n div 2) do write (' ');
+            write ('*');
+          end;
+        if (i > ((n div 2)+1)) and (i <> n) then 
+          begin
+            writeln;
+            for k := 1 to ((n div 2) -n+i) do write(' ');
+            write('*');
+            for k := 1 to n-(2+2*(n div 2 -n+i)) do write(' ');
+            write('*');
+          end; 
+        if (i <= (n div 2)) and (i <> 1) then 
+          begin
+            writeln;
+            for k := 1 to ((n div 2) -i+1) do write(' ');
+            write('*');
+            for k := 1 to n-(2+2*(n div 2 -i+1)) do write(' ');
+            write('*');
+          end;
+        if i = ((n div 2)+1) then 
+          begin
+            writeln;
+            write('*');
+            for k := 1 to n - 2 do write(' ');
+            write('*');
+          end;
+      end;
+   writeln;   		
+end.
