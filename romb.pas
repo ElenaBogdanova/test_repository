bbb01c2a92a9aa13c85a1da3eb2d2ad9eb95ddb5
ElenaBogdanova <elena_12_00@bk.ru>
2017-10-08 23:36:40 +0300

    change2

--- romb.pas
+++ romb.pas
@@ -9,36 +9,30 @@ begin
       writeln;
       if (i = 1) or (i = n) then 
       begin
-         for k := 1 to (n div 2) do
-         write (' ');
+         for k := 1 to (n div 2) do write (' ');
          write ('*');
       end;
       if (i > ((n div 2)+1)) and (i <> n) then 
          begin
             writeln;
-            for k := 1 to ((n div 2) -n+i) do
-            write(' ');
+            for k := 1 to ((n div 2) -n+i) do write(' ');
             write('*');
-            for k := 1 to n-(2+2*(n div 2 -n+i)) do
-            write(' ');
+            for k := 1 to n-(2+2*(n div 2 -n+i)) do write(' ');
             write('*');
          end; 
       if (i <= (n div 2)) and (i <> 1) then 
       begin
          writeln;
-         for k := 1 to ((n div 2) -i+1) do
-         write(' ');
+         for k := 1 to ((n div 2) -i+1) do write(' ');
          write('*');
-         for k := 1 to n-(2+2*(n div 2 -i+1)) do
-         write(' ');
+         for k := 1 to n-(2+2*(n div 2 -i+1)) do write(' ');
          write('*');
       end;
       if i = ((n div 2)+1) then 
       begin
          writeln;
          write('*');
-         for k := 1 to n - 2 do
-         write(' ');
+         for k := 1 to n - 2 do write(' ');
          write('*');
       end;
    end;
